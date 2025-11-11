7bec9a2c7a3f221e19b203e48084289b84bb8786
yongyoujin25-sudo <yongyoujin25@gmail.com>
2025-11-11 23:31:10 +0900

    무버 클래스와 예제 2.1 불러오기

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
+let mover;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(640, 240);
+  mover = new Mover();
+  createP("Click mouse to apply wind force.");
 }
 
 function draw() {
-  background(220);
+  background(255);
+
+  let gravity = createVector(0, 0.1);
+  mover.applyForce(gravity);
+
+  if (mouseIsPressed) {
+    let wind = createVector(0.1, 0);
+    mover.applyForce(wind);
+  }
+
+  mover.update();
+  mover.show();
+  mover.checkEdges();
 }
